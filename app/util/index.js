4523c8b2abd8d80294c388d002aa2f0ba44e5330
Ilya Kolbin <iskolbin@gmail.com>
2017-04-21 16:35:58 +0300

    fix

--- app/util/index.js
+++ app/util/index.js
@@ -1,10 +1,5 @@
 import range from './range'
 import random from './random'
 import flatten from './flatten'
-import cancelFullscreen from './cancelFullscreen'
-import isFullscreen from './isFullscreen'
-import requestFullscreen from './requestFullscreen'
-import fullscreenElement from './fullscreenElement'
-import addFullscreenChangeListener from './addFullscreenChangeListener'
 
-export {range, random, flatten, cancelFullscreen, isFullscreen, requestFullscreen, fullscreenElement, addFullscreenChangeListener}
+export {range, random, flatten}
